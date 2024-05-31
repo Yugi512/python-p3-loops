0cff6a6311987da10ed4695c3a1a613fccbf0368
Kelvin Gil <gilkelvin2@gmail.com>
2024-05-31 14:36:59 -0400

    finished lab

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,23 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    i = 10
+    while 1 <= i:
+        print(f"{i}")
+        print("Happy New Year!")
+        i -= 1
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    int_list = [ints * ints for ints in int_list ]
+    return int_list
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for i in range(1,101):
+        if i%15 == 0:
+            print("FizzBuzz")
+        elif i % 5 == 0:
+            print("Buzz")
+        elif i%3 == 0:
+            print("Fizz")
+        else :
+            print(f"{i}")
